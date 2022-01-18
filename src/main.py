99af7023dbcad5ec267b9722818e5561a840bb6b
tokudaek@brucutuvi <tokudaek@gmail.com>
2022-01-18 11:34:49 -0300

    Beautify plots

--- src/main.py
+++ src/main.py
@@ -33,7 +33,6 @@ def interiority(dataorig):
 def jaccard(dataorig, a):
     """Calculate the interiority index of the two rows. @vs has 2rows and n-columns, where
     n is the number of features"""
-    # info(inspect.stack()[0][3] + '()')
     data = np.abs(dataorig)
     den = np.sum(np.max(data, axis=0))
     datasign = np.sign(dataorig)
@@ -43,24 +42,34 @@ def jaccard(dataorig, a):
     sminus = np.sum(minus_ * np.min(data, axis=0))
     num = a * splus - (1 - a) * sminus
     return num / den
-    
 
 ##########################################################
 def coincidence(data, a):
     inter = interiority(data)
     jac = jaccard(data, a)
-    # print(inter, jac)
     return inter * jac
 
 ##########################################################
-def plot_weighted_graph(adjorig, ethresh, outpath):
+def plot_weighted_graph(adjorig, labels, ethresh, outpath):
     adj = np.tril(adjorig.copy()) # Lower triangle, to avoid double edges
     adj[adj < ethresh] = 0
     g = igraph.Graph(n=len(adjorig), directed=False)
     xx, yy = np.where(adj != 0)
+    ewidths = []
     for x, y in zip(xx, yy):
         g.add_edge(x, y, w=adj[x, y])
-    igraph.plot(g, outpath)
+        ewidths.append(adj[x, y])
+    ewidths = np.array(ewidths)
+    wmax = 4 # max edge widths
+    erange = np.max(ewidths) - np.min(ewidths)
+
+    if erange == 0: # All edges have the same weight
+        ewidths = np.ones(len(ewidths))
+    else:
+        ewidths = ( (ewidths - np.min(ewidths)) / erange ) * wmax
+
+    igraph.plot(g, outpath, vertex_frame_width=0, vertex_label=labels, edge_width=ewidths,
+                                            vertex_color='yellow')
 
 ##########################################################
 def get_coincidence_graph(dataorig, alpha, standardize):
@@ -95,23 +104,24 @@ def main(outdir):
     data = df[feats].to_numpy()
     n, m = data.shape
 
-    labels = []
-    datameta = []
-    for mm in range(1, m +1):
+    datameta = [] # Each row will correspond to a flattened adj matrix
+    labels = [] # Label of each row of datameta
+
+    for mm in range(1, m +1): # Size of the subset varies from 1 to m
         combs = list(combinations(range(m), mm))
         for comb in combs:
             combids = list(comb)
             suff = '_'.join([str(ind) for ind in combids])
             adj = get_coincidence_graph(data[:, combids], alpha, True)
             plotpath = pjoin(outdir, suff + '.png')
-            plot_weighted_graph(adj, edgethresh, plotpath)
+            vstr = [str(ii) for ii in range(n)]
+            plot_weighted_graph(adj, vstr, edgethresh, plotpath)
             datameta.append(adj.flatten())
             labels.append(suff)
 
     datameta = np.array(datameta)
     adj = get_coincidence_graph(datameta, alpha, False)
-    plotpath = pjoin(outdir, 'meta.png')
-    plot_weighted_graph(adj, edgethresh, plotpath)
+    plot_weighted_graph(adj, labels, edgethresh, pjoin(outdir, 'meta.png'))
 
 ##########################################################
 if __name__ == "__main__":
