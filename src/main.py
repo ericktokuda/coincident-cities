259fa002ff2121d6504839c31434038838027298
tokudaek@brucutuvi <tokudaek@gmail.com>
2022-01-29 12:06:54 -0300

    Shrink everything to one single script

--- src/main.py
+++ src/main.py
@@ -14,10 +14,116 @@ import numpy as np
 import matplotlib; matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 from myutils import info, create_readme
+from myutils.transform import pca, get_pc_contribution
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
 from itertools import combinations
 import igraph
+
+##########################################################
+def plot_pca(df, outdir):
+    """Short description """
+    info(inspect.stack()[0][3] + '()')
+    cols = [ 'degmean', 'degstd', 'deg3', 'deg4', 'deg5', 'transmean', 'transstd', 'eangstd',
+    'vposstd2', 'lacun21', 'acc05mean', 'acc05std']
+
+    df2 = df[cols].copy(deep=True)
+    data = df2.to_numpy()
+    tr, evecs, evals = pca(data, normalize=True)
+    pcs, contribs, relcontribs = get_pc_contribution(evecs, evals)
+    info('Contribs:', np.array(cols)[pcs], contribs, relcontribs)
+
+    W = 640; H = 480
+    fig, ax = plt.subplots(figsize=(W*.01, H*.01), dpi=100)
+    # ax.set_title('PCA components (pc1 and pc2)')
+
+    cities = [ c.capitalize() for c in list(df.city)]
+
+    ax.scatter(tr[:, 0], tr[:, 1], s=380, c='#809fff', edgecolors='#dddddd')
+    # for i in range(tr.shape[0]):
+        # ax.scatter(tr[i, 0], tr[i, 1], label=cities[i])
+
+    for pos in ['right', 'top']:
+    # for pos in ['right', 'top', 'bottom', 'left']:
+        ax.spines[pos].set_visible(False)
+
+    citylabels = [chr(i) for i in range(97, 97 + 21)]
+    for i in range(len(df2)):
+        # ax.annotate(df.iloc[i].city.capitalize(), (tr[i, 0], tr[i, 1]))
+        ax.annotate(citylabels[i], (tr[i, 0], tr[i, 1]), ha='center', va='center')
+        # ax.annotate(citylabels[i], (tr[i, 0]-.005, tr[i, 1]))
+
+    xylim = np.max(np.abs(tr[:, 0:2])) * 1.1
+    # ax.set_xlabel('PCA1 ({} ({}%)'.format(cols[pcs[0]], int(contribs[0] * 100)))
+    # ax.set_ylabel('PCA2 {}: ({}%)'.format(cols[pcs[1]], int(contribs[1] * 100)))
+    ax.set_xlabel('PC 1')
+    ax.set_ylabel('PC 2')
+    # ax.set_xlim(-.2, +.65)
+    # ax.set_ylim(-xylim, +xylim)
+    # plt.legend(bbox_to_anchor=(1.05, 1))
+    plt.tight_layout()
+    # plt.legend()
+
+    outpath = pjoin(outdir, 'pca.pdf')
+    plt.savefig(outpath)
+
+
+##########################################################
+def plot_correlogram(df, outdir):
+    """Plot heatmap"""
+    info(inspect.stack()[0][3] + '()')
+    cols = ['degstd', 'transstd', 'vposstd2', 'lacun21', 'acc05std']
+
+    df2 = df[cols].copy(deep=True)
+    data = df2.to_numpy()
+
+    W = 640; H = 480
+    fig, ax = plt.subplots(figsize=(W*.01, H*.01), dpi=100)
+
+    corrs = df2.corr()
+
+    import seaborn as sns
+    sns.pairplot(corrs, corner=True)
+    plt.tight_layout()
+
+    outpath = pjoin(outdir, 'pairwise_correl.pdf')
+    plt.savefig(outpath)
+
+##########################################################
+def plot_histograms(outdir):
+    """Plot histograms"""
+    info(inspect.stack()[0][3] + '()')
+
+    n = 5
+    clusters = [
+        [[3,5], [1,3,5], [2,3,5], [3,4,5], [1,3,4,5], [1,2,3,5], [2,3,4,5], [1,2,3,4,5]],
+        [[1,3,4], [3,4], [1,2,3,4], [2,3,4], [1,2,3], [2,3], [1,3]],
+        [[5], [4, 5], [2, 4, 5], [1,4,5], [1,2,5], [2,5], [1,2,4,5], [1,5]],
+        [[1,2,4], [1,4], [1,2], [2,4], [4]]
+    ]
+
+    for clid, cluster in enumerate(clusters):
+        counter = np.zeros(n, dtype=float)
+        clsizes = []
+        for i, nodes in enumerate(cluster):
+            clsizes.append(len(nodes))
+            for node in nodes:
+                counter[node - 1] += 1
+
+        W = 320; H = 240
+
+        fig, ax = plt.subplots(figsize=(W*.01, H*.01), dpi=100)
+        # ax.bar(range(1, n+1), np.array(counter) / np.sum(counter))
+        ax.bar(range(1, n+1), np.array(counter), color='#66BB6A')
+        ax.set_xlabel('Feature id')
+        ax.set_ylim(0, 9)
+        plt.tight_layout()
+
+        outpath = pjoin(outdir, 'meta_hist{}.pdf'.format(clid))
+        plt.savefig(outpath)
+
+##########################################################
+##########################################################
 
 ##########################################################
 def interiority(dataorig):
@@ -142,6 +248,45 @@ def plot_heatmaps(adjdir, outdir):
         plt.close()
 
 ##########################################################
+def compute_coincidence_rows(df, edgethresh1):
+    """Calculate coincidence index of the rows"""
+    if idcol == None:
+        rowids = [str(i) for i in range(len(df))]
+    else:
+        rowids = df[idcol]
+
+    if featcols == None:
+        data = df.to_numpy()
+        feats = df.columns
+    else:
+        feats = featcols.split(',')
+        data = df[feats].to_numpy()
+
+    n, m = data.shape
+
+    datameta = [] # Each row will correspond to a flattened adj matrix
+    labels = [] # Label of each row of datameta
+
+    # rowlabels = [rowid.capitalize() for rowid in df.city]
+    rowlabels = [chr(i) for i in range(97, 97+n)]
+
+    # m = 1
+    for mm in range(1, m +1): # Size of the subset varies from 1 to m
+        # combs = [list(range(12))]
+        combs = list(combinations(range(m), mm))
+        for comb in combs:
+            combids = list(comb)
+            suff = '_'.join([str(ind+1) for ind in combids])
+            info('Combination ', suff)
+            # suff = '_'.join([feats[ind] for ind in combids])
+            adj = get_coincidence_graph(data[:, combids], alpha, True)
+            np.savetxt(pjoin(outdir, '{}.txt'.format(suff)), adj)
+            plotpath = pjoin(outdir, suff + '.pdf')
+            plot_weighted_graph(adj, rowlabels, edgethresh1, 'fr', plotpath)
+            datameta.append(adj.flatten())
+            labels.append(suff)
+
+##########################################################
 def main(csvpath, idcol, featcols, outdir):
     """Short description"""
     info(inspect.stack()[0][3] + '()')
@@ -149,6 +294,8 @@ def main(csvpath, idcol, featcols, outdir):
     seed = 6
     random.seed(seed); np.random.seed(seed)
     alpha = .6
+
+    # compute_coincidence_rows()
     edgethresh1 = .2
     edgethresh2 = .5
 
@@ -205,6 +352,10 @@ def main(csvpath, idcol, featcols, outdir):
     # plot_weighted_graph(adj, vweights, edgethresh2, pjoin(outdir, 'weights.pdf'))
 
     plot_heatmaps(outdir, pjoin(outdir, 'heatmaps/'))
+
+    plot_pca(df, outdir)
+    plot_histograms(outdir)
+    plot_correlogram(df, outdir)
 
 ##########################################################
 if __name__ == "__main__":
